05b25ac75ec6d773f24cf4ee7d65d48885ed3014
Akash <akkida746@gmail.com>
2020-06-07 11:20:59 +0530

    Eureka Discovery Service and Client registration

---
+++ DemoService/src/main/java/com/example/training/DemoServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.training;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner runner() {
+		return Args ->{
+			System.out.println("Demo serice started..");
+		};
+	}
+
+}
---
+++ ServiceDiscovery/src/main/java/com/example/training/ServiceDiscoveryApplication.java
@@ -0,0 +1,23 @@
+package com.example.training;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner () {
+		return args -> {
+			System.out.println("Eureka service started..."); 
+		};
+	}
+}
